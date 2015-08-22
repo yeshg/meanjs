799be71dbf99ad8dd18fdf9897ffacb7ad4235f9
yeshg <yesh.godse@gmail.com>
2015-08-21 17:36:57 -0700

    Day 24 before Seatle

--- public/modules/customers/controllers/customers.client.controller.js
+++ public/modules/customers/controllers/customers.client.controller.js
@@ -12,6 +12,36 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authe
         // Find a list of Customers
         this.customers = Customers.query();
 
+
+        this.modalCreate = function (size) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'modules/customers/views/create-customer.client.view.html',
+                controller: function ($scope, $modalInstance) {
+
+                    $scope.ok = function (IsValid) {
+                        if (IsValid) {
+                            $modalInstance.close($scope.customer);
+                        }
+                    };
+
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                        location.reload();                  // Because we need to prove to user db has not been changed
+                    };
+
+                },
+                size: size
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+
+
         this.modalUpdate = function (size, selectedCustomer) {
 
             var modalInstance = $modal.open({
@@ -20,7 +50,7 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authe
                     $scope.customer = customer;
 
                     $scope.ok = function (IsValid) {
-                        if (IsValid){
+                        if (IsValid) {
                             $modalInstance.close($scope.customer);
                         }
                     };
@@ -51,6 +81,39 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authe
 
 customersApp.controller('CustomersCreateController', ['$scope', 'Customers',
     function ($scope, Customers) {
+        // Create new Customer
+        $scope.create = function () {
+            // Create new Customer object
+            var customer = new Customers({
+                firstName: this.firstName,
+                surname: this.surname,
+                suburb: this.suburb,
+                country: this.country,
+                industry: this.industry,
+                email: this.email,
+                phone: this.phone,
+                referred: this.referred,
+                channel: this.channel
+            });
+
+            // Redirect after save
+            customer.$save(function (response) {
+                $location.path('customers/' + response._id);
+
+                // Clear form fields
+                $scope.fisrtName = '';
+                $scope.surname = '';
+                $scope.suburb = '';
+                $scope.industry = '';
+                $scope.email = '';
+                $scope.phone = '';
+                $scope.referred = '';
+                $scope.channel = '';
+
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
     }
 ]);
@@ -68,75 +131,41 @@ customersApp.controller('CustomersUpdateController', ['$scope', 'Customers',
     }
 ]);
 
+/*
+// Remove existing Customer
+$scope.remove = function (customer) {
+    if (customer) {
+        customer.$remove();
 
-/*// Create new Customer
- $scope.create = function () {
- // Create new Customer object
- var customer = new Customers({
- firstName: this.firstName,
- surname: this.surname,
- suburb: this.suburb,
- country: this.country,
- industry: this.industry,
- email: this.email,
- phone: this.phone,
- referred: this.referred,
- channel: this.channel
- });
+        for (var i in $scope.customers) {
+            if ($scope.customers [i] === customer) {
+                $scope.customers.splice(i, 1);
+            }
+        }
+    } else {
+        $scope.customer.$remove(function () {
+            $location.path('customers');
+        });
+    }
+};
 
- // Redirect after save
- customer.$save(function (response) {
- $location.path('customers/' + response._id);
+// Update existing Customer
+$scope.update = function () {
+    var customer = $scope.customer;
 
- // Clear form fields
- $scope.fisrtName = '';
- $scope.surname = '';
- $scope.suburb = '';
- $scope.industry = '';
- $scope.email = '';
- $scope.phone = '';
- $scope.referred = '';
- $scope.channel = '';
-
- }, function (errorResponse) {
- $scope.error = errorResponse.data.message;
- });
- };
-
- // Remove existing Customer
- $scope.remove = function (customer) {
- if (customer) {
- customer.$remove();
-
- for (var i in $scope.customers) {
- if ($scope.customers [i] === customer) {
- $scope.customers.splice(i, 1);
- }
- }
- } else {
- $scope.customer.$remove(function () {
- $location.path('customers');
- });
- }
- };
-
- // Update existing Customer
- $scope.update = function () {
- var customer = $scope.customer;
-
- customer.$update(function () {
- $location.path('customers/' + customer._id);
- }, function (errorResponse) {
- $scope.error = errorResponse.data.message;
- });
- };
+    customer.$update(function () {
+        $location.path('customers/' + customer._id);
+    }, function (errorResponse) {
+        $scope.error = errorResponse.data.message;
+    });
+};
 
 
-
- // Find existing Customer
- $scope.findOne = function () {
- $scope.customer = Customers.get({
- customerId: $stateParams.customerId
- });
- };*/
+// Find existing Customer
+$scope.findOne = function () {
+    $scope.customer = Customers.get({
+        customerId: $stateParams.customerId
+    });
+};
+*/
 
